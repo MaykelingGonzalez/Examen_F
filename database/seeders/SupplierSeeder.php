91edd9e4b6f294ab50c52b75ef34f3ae3aa39f98
MaykelingG <maykelingg308@gmail.com>
2025-08-27 11:27:39 -0600

    feat(Supplier): creation of Seeder Supplier

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -17,32 +17,62 @@ class SupplierSeeder extends Seeder
         Supplier::create([
             'supplier_code' => 'PROV-0001-001',
             'supplier_type' => 'Mayorista',
-            'company' => 'Ferretería Jenny S.A',
+            'company' => 'Ferretería Jenny',
             'representative' => 'Fernando Moisés Duarte Osorio',
             'telephone' => '+505 8918-7562',
-            'email' => 'proveedor1@example.com',
-            'address' => 'Costado Sureste Escuela el Rosario 1c al Sur y 1/2C al este',
+            'email' => 'ferreteriajenny20@gmail.com',
+            'address' => 'De la rotonda El Güegüense 3 cuadras al oeste, Managua, Nicaragua.',
         ]);
 
         Supplier::create([
             'supplier_code' => 'PROV-0001-002',
             'supplier_type' => 'Minorista',
-            'company' => 'Ferretería Juan S.A',
-            'representative' => 'Juan Pérez',
+            'company' => 'FETESA',
+            'representative' => 'Juan Marcelo Pérez Cruz',
             'telephone' => '+505 1234-5678',
-            'email' => 'proveedor2@example.com',
-            'address' => 'Costado Noreste Escuela el Rosario 1c al Sur y 1/2C al este',
+            'email' => 'fetesa2021@gmail.com',
+            'address' => 'Del parque central 2 cuadras al norte, 1 cuadra al este, León, Nicaragua.',
         ]);
 
         Supplier::create([
             'supplier_code' => 'PROV-0001-003',
             'supplier_type' => 'Mayorista',
-            'company' => 'Ferretería Pedro S.A',
-            'representative' => 'Pedro López',
+            'company' => 'DIMACO',
+            'representative' => 'Pedro Manuel López Rivas',
             'telephone' => '+505 9876-5432',
-            'email' => 'proveedor3@example.com',
-            'address' => 'Costado Sureste Escuela el Rosario 1c al Sur y 1/2C al este',
-
+            'email' => 'dimaco002@gmail.com',
+            'address' => 'Del mercado municipal 2 cuadras al oeste, 1 cuadra al sur, Granada, Nicaragua.',
         ]);
+
+        Supplier::create([
+            'supplier_code' => 'PROV-0001-004',
+            'supplier_type' => 'Minorista',
+            'company' => 'Ferretería La Económica',
+            'representative' => 'Carlos Manuel Rodríguez Osorio',
+            'telephone' => '+505 2468-1357',
+            'email' => 'laeconomica_1@example.com',
+            'address' => 'Del colegio San Francisco 1 cuadra al oeste, 2 cuadras al norte, Ocotal, Nicaragua.',
+        ]);
+
+        Supplier::create([
+            'supplier_code' => 'PROV-0001-005',
+            'supplier_type' => 'Mayorista',
+            'company' => 'GRUPO SINSA',
+            'representative' => 'Eva María Cruz Torrez',
+            'telephone' => '+505 1357-2468',
+            'email' => 'sinsa_of1@example.com',
+            'address' => 'De la gasolinera UNO 1 cuadra al este, 1 cuadra al norte, Jinotepe, Nicaragua.',
+        ]);
+
+        Supplier::create([
+            'supplier_code' => 'PROV-0001-006',
+            'supplier_type' => 'Minorista',
+            'company' => 'CEMEX NICARAGUA',
+            'representative' => 'Ana Lucía Torres Pérez',
+            'telephone' => '+505 6543-2109',
+            'email' => 'cemex_nicaragua@example.com',
+            'address' => 'De los semáforos del Zumen, 3 cuadras al oeste, Managua, Nicaragua',
+        ]);
+        
     }
 }
