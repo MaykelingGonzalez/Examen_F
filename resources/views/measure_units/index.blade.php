0151c679cc498b6ea2ef894b8e302f78174404a7
MaykelingG <maykelingg308@gmail.com>
2025-08-27 07:19:30 -0600

    fix(measure_units): update in measure_units view

--- resources/views/measure_units/index.blade.php
+++ resources/views/measure_units/index.blade.php
@@ -7,7 +7,7 @@
             <div class="card shadow">
                 <div class="card-header border-0">
                     <div class="d-flex justify-content-between align-items-center">
-                        <h3 class="mb-0">Unidades de Medida</h3>
+                        <h3 class="mb-0"><b>Unidades de Medida</b></h3>
 
                         <div class="btn-group mt-2 mt-md-0" role="group" aria-label="Botones de acción">
 
